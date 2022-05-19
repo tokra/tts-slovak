e1ff349699baddc9c3ca17ef2ae7d9a12ca80378
AntonFivaldy <92924530+AntonFivaldy@users.noreply.github.com>
2022-05-19 17:32:18 +0200

    Update SynthesisSlovak.java

--- src/main/java/io/tokra/audio/tts/slovak/SynthesisSlovak.java
+++ src/main/java/io/tokra/audio/tts/slovak/SynthesisSlovak.java
@@ -49,6 +49,7 @@ public final class SynthesisSlovak extends Synthesis {
 			// Iterating the args array
 			// using for each loop
 			// using for each loop istv_l@N621LIS-2 MINGW64 ~/git/tts-slovak (FEATURE_2) -> merge to main
+			//cohfo
 			for (String val : args) {
 				// Printing command line arguments
 				System.out.println(val);
